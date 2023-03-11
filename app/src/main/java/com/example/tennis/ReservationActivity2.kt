f336d184581fc49725ec9ec95cc0115f2004ff22
schemsmak <schems.makhlouf@isen.yncrea.fr>
2023-03-11 16:13:38 +0100

    condition terrain2

--- app/src/main/java/com/example/tennis/ReservationActivity2.kt
+++ app/src/main/java/com/example/tennis/ReservationActivity2.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TimePicker
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.ktx.database
@@ -73,22 +74,58 @@ class ReservationActivity2 : AppCompatActivity() {
             // Vérification de la date sélectionnée
             val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
             val hour = timePicker2.hour
-            val terrain = "terrain1"
+            val terrain = "terrain2"
+
             if (dayOfWeek == Calendar.SATURDAY && hour >= 10 && hour < 18) {
-                Toast.makeText(this, "Réservation impossible le samedi entre 10h et 18h", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Réservation impossible le samedi entre 10h et 18h",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else if (hour >= 22) {
+                Toast.makeText(
+                    this,
+                    "Réservation impossible après 22h",
+                    Toast.LENGTH_SHORT
+                ).show()
+
             } else {
-                // Récupération de la date sélectionnée sous string
-                val selectedDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(calendar.time)
-                Log.d("date", selectedDate)
-                if (currentUser != null) {
-                    database.child("users").child(currentUser.uid).child("date").setValue(selectedDate)
-                    database.child("users").child(currentUser.uid).child("hour").setValue(hour)
-                    database.child("users").child(currentUser.uid).child("terrain").setValue(terrain)
+                // Création d'une boîte de dialogue pour choisir la durée de la réservation
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Choisir la durée de la réservation")
+                val durations = arrayOf("1 heure", "2 heures maximum")
+                builder.setItems(durations) { _, which ->
+                    val duration = when (which) {
+                        0 -> 1
+                        1 -> 2
+                        else -> 1
+                    }
+
+                    // Récupération de la date sélectionnée sous string
+                    val selectedDate =
+                        SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(calendar.time)
+                    Log.d("date", selectedDate)
+                    if (currentUser != null) {
+                        database.child("users").child(currentUser.uid).child("date")
+                            .setValue(selectedDate)
+                        database.child("users").child(currentUser.uid).child("hour").setValue(hour)
+                        database.child("users").child(currentUser.uid).child("duration")
+                            .setValue(duration)
+                        database.child("users").child(currentUser.uid).child("terrain")
+                            .setValue(terrain)
+                    }
+                    Toast.makeText(
+                        this,
+                        "Terrain 2 réservé pour $duration heure(s)",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                    intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
                 }
-                Toast.makeText(this, "Terrain 2 Reservé", Toast.LENGTH_SHORT).show()
-                intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
+                builder.show()
             }
         }
     }
 }
+
